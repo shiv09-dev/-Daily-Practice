a658c920b9aa257e32b68b51c2ae6e687de59749
shiv09-dev <daveshivam09@gmail.com>
2025-06-13 13:39:16 +0530

    Add files via upload

---
+++ Control_statement.c
@@ -0,0 +1,41 @@
+//Control Statement
+
+#include<stdio.h>
+int main()
+{
+	int celsius = 35 , fahrenheit;
+	int age = 25 , weight = 60;
+	
+	if(celsius >30)
+	{
+	fahrenheit = celsius * 9/5 +32;
+	printf("Temeprature from celsius to fahrenheit is : %d \n \n" , fahrenheit);
+	
+	}
+	else
+	{
+		printf("Temperature is less than 30 degree celsius \n n\n");
+	}
+	
+	
+	if(age > 18)
+	{
+		printf("Your age condition is satisfied , you are eligible to donate blood \n \n ");
+		
+		if(weight >50)
+		
+			printf("You wieght condition is satisfied \n \n");
+		
+		else
+			printf("Your weight condition is not satisfied \n \n");
+			
+	}
+	else
+		{
+			printf("Your age  consition is not satisifed , your are not eligible to donate blood");
+		}
+		
+	printf("Thank you for donating blood");
+}
+
+	
