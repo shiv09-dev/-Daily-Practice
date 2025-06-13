089d2ecd2f338f4dfa1ca16c7be6a7786ca42d35
shiv09-dev <daveshivam09@gmail.com>
2025-06-13 13:49:40 +0530

    Update Control_statement.c

--- Control_statement.c
+++ Control_statement.c
@@ -6,7 +6,7 @@ int main()
 	int celsius = 35 , fahrenheit;
 	int age = 25 , weight = 60;
 	
-	printf("Checking condition to convert clesius to fahrenheit");
+	printf("Checking condition to convert clesius to fahrenheit \n \n");
 	if(celsius >30)
 	{
 	fahrenheit = celsius * 9/5 +32;
@@ -18,7 +18,7 @@ int main()
 		printf("Temperature is less than 30 degree celsius \n \n");
 	}
 	
-	printf("Checking conditon if you can donate blood or not \n");
+	printf("Checking conditon if you can donate blood or not \n \n");
 	if(age > 18)
 	{
 		printf("Your age condition is satisfied , you are eligible to donate blood \n \n ");
