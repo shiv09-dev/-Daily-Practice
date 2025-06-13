9983d18f802833b7ddd3e5bca50b206964795e20
shiv09-dev <daveshivam09@gmail.com>
2025-06-13 13:57:10 +0530

    Update Control_statement.c

--- Control_statement.c
+++ Control_statement.c
@@ -6,7 +6,7 @@ int main()
 	int celsius = 35 , fahrenheit;
 	int age = 25 , weight = 60;
 	
-	printf("Checking condition to convert clesius to fahrenheit : \n \n");
+	printf("Checking condition to convert clesius to fahrenheit : \n ");
 	if(celsius >30)
 	{
 	fahrenheit = celsius * 9/5 +32;
@@ -18,25 +18,25 @@ int main()
 		printf("Temperature is less than 30 degree celsius \n \n");
 	}
 	
-	printf("Checking conditon if you are eligible to donate blood : \n \n");
+	printf("Checking conditon if you are eligible to donate blood : \n ");
 	if(age > 18)
 	{
-		printf("Your age condition is satisfied \n \n ");
+		printf("Your age condition is satisfied \n ");
 		
 		if(weight >50)
 		
-			printf("You weight condition is also satisfied , you are eligible to donate blood \n \n");
+			printf("You weight condition is also satisfied , you are eligible to donate blood \n ");
 		
 		else
-			printf("Your weight condition is not satisfied , your are not eligible to donate blood \n \n");
+			printf("Your weight condition is not satisfied , your are not eligible to donate blood \n");
 			
 	}
 	else
 		{
-			printf("Your age condition is not satisifed , your are not eligible to donate blood");
+			printf("Your age condition is not satisifed , your are not eligible to donate blood \n");
 		}
 		
-	printf("Thank you for donating blood");
+	printf("Thank you for donating blood \n \n ");
 }
 
 	
